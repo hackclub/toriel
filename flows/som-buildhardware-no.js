a65e3c66ef1e7e4b3eaa81a7a913d81b699c527d
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-11 17:24:46 -0400

    fix blocks

--- flows/som-buildhardware-no.js
+++ flows/som-buildhardware-no.js
@@ -5,7 +5,7 @@ const loadFlow = app => {
   app.action('buildhardware_no', async ({ ack, body }) => {
     await ack()
     await setFlow(body.user.id, 'SOM - Build Hardware No')
-    await updateSingleBlockMessage(app, body.message.ts, body.channel.id, `Are you building a hardware project during the Summer of Making?`)
+    await updateSingleBlockMessage(app, body.message.ts, body.channel.id, `Are you building a hardware project during the Summer of Making?`, 'No', 'mimmiggie')
     await sendMessage(app, body.channel.id, '...', 1000)
     await sendMessage(app, body.channel.id, '...', 1000)
     await sendDoubleBlockMessage(app, body.channel.id, `Do you know what you want to work on?`, 'Yes', 'No', 'work_yes', 'work_no')
