3d5433c4831263a967026850509404a7ee32b0cf
Matthew Stanciu <mattbstanciu@gmail.com>
2020-06-22 19:27:39 -0400

    ???

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const { hasPushedButton, hasCompletedTutorial, getIslandId,
 const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET })
 
 const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
   receiver
 });
