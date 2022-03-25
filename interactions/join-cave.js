cc132f9f0a9eccaca97864851763cdbd72f11a50
Max Wofford <max@maxwofford.com>
2022-03-25 15:40:13 -0400

    Add user inviting and placeholder instructional steps

---
+++ interactions/join-cave.js
@@ -0,0 +1,68 @@
+// the user joins #the-cave, the starter channel
+const { inviteUserToChannel } = require("../util/invite-user-to-channel");
+const { sleep } = require("../util/sleep");
+const { transcript } = require("../util/transcript");
+
+async function join(args) {
+  const { client, payload } = args
+  const { user, channel } = payload
+  await client.chat.postEphemeral({
+    text: transcript('cave-join', { user }),
+    channel: transcript('channels.cave'),
+    user,
+  })
+
+  await sleep(1000)
+
+  await client.chat.postMessage({
+    text: transcript('house.hello'),
+    // icon_url: transcript('avatar.default'),
+    channel: user,
+  })
+
+  await sleep(1000)
+
+  await client.chat.postMessage({
+    text: transcript('house.venture'),
+    // icon_url: transcript('avatar.sad'),
+    channel: user,
+  })
+  await client.chat.postMessage({
+    text: transcript('house.beforeGo'),
+    // icon_url: transcript('avatar.default'),
+    channel: user,
+  })
+
+  await inviteUserToChannel(client, user, transcript('channels.slack-themes'), true)
+  await sleep(1000)
+
+  await client.chat.postMessage({
+    text: transcript('house.theme'),
+    // icon_url: transcript('avatar.default'),
+    channel: user,
+  })
+
+  await client.chat.postMessage({
+    blocks: [transcript('block.single-button', {text: "Continue", value: "theme_complete"})],
+    // icon_url: transcript('avatar.log'),
+    channel: user,
+  })
+
+  // await client.chat.postMessage({
+  //   text: transcript('house.basement'),
+  //   blocks: [
+  //     {
+  //       type: 'context',
+  //       elements: [
+  //         {
+  //           type: 'mrkdwn',
+  //           text: transcript('house.basement'),
+  //         },
+  //       ],
+  //     },
+  //   ],
+  //   // icon_url: transcript('avatar.sad'),
+  //   channel: user,
+  // })
+}
+module.exports = { joinInteraction: join }
---
+++ util/invite-user.js
@@ -0,0 +1,24 @@
+// for testing: send me an email and I'll invite them to slack with all the right settings
+
+const fetch = require("node-fetch")
+const { transcript } = require("./transcript")
+
+async function inviteUser(email) {
+  const channels = [ transcript('channels.cave') ]
+  const customMessage = "While wandering through a forest, you stumble upon a cave..."
+  const params = [
+    `email=${email}`,
+    `token=${process.env.SLACK_LEGACY_TOKEN}`,
+    // `real_name=${data.name}`,
+    'restricted=true',
+    `channels=${channels.join(',')}`,
+    `custom_message=${customMessage}`,
+    'resend=true'
+  ].join('&')
+  const url = `https://slack.com/api/users.admin.invite?${params}`
+  await fetch(url, { method: 'POST' })
+    .then(r => r.json())
+    .then(r => console.log('Slack response', r))
+}
+
+module.exports = { inviteUser }
