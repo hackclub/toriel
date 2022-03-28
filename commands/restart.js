1288b11534d466c0f061ceaa69d867c943ead008
Max Wofford <max@maxwofford.com>
2022-03-28 12:51:51 -0400

    Admins can restart toriel flow

--- commands/restart.js
+++ commands/restart.js
@@ -2,29 +2,47 @@ const { joinInteraction } = require('../interactions/join-cave')
 const { transcript } = require('../util/transcript')
 
 async function restart(args) {
-  console.log('running here')
   const { payload, client, respond } = args
-  const { user_id } = payload
+  const { user_id, text } = payload
 
-  respond({
-    text: 'restarting...',
-  })
+  let userToReset = user_id
 
-  console.log({
-    payload,
-    text: 'restarting...',
-    channel: user_id,
-    // icon_url: transcript('startup.avatar')
-  })
+  const userRegex = /<@([A-Za-z0-9]+)\|.+>/i
+  const userMatches = text.match(userRegex)
+  const foundUser = userMatches ? userMatches[1] : null
+  if (foundUser && foundUser != '') {
+    const callingUser = await client.users.info({
+      user: user_id
+    })
+
+    if (callingUser.user.is_admin || callingUser.user.is_owner) {
+      userToReset = foundUser
+
+      respond({
+        text: `resetting <@${userToReset}>`,
+      })
+    } else {
+      // no permissions– skip
+      respond({
+        text: 'Only admins and owners can reset another user',
+      })
+      return null
+    }
+  } else {
+    respond({
+      text: `resetting your tutorial...`,
+    })
+  }
+
   await client.chat.postMessage({
     text: 'restarting...',
-    channel: user_id,
+    channel: userToReset,
     // icon_url: transcript('startup.avatar')
   })
 
   const joinArgs = args
   joinArgs.payload.channel = transcript('channels.cave')
-  joinArgs.payload.user = user_id
+  joinArgs.payload.user = userToReset
   joinInteraction(args)
 }
 module.exports = restart
