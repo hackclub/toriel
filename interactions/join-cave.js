8c866f4615825dcca75f84b5dac975bdc0a29187
Max Wofford <max@maxwofford.com>
2022-03-30 10:14:05 -0400

    Improve timing on slack themes step

--- interactions/join-cave.js
+++ interactions/join-cave.js
@@ -6,73 +6,79 @@ const { transcript } = require('../util/transcript')
 async function joinCaveInteraction(args) {
   const { client, payload } = args
   const { user, channel } = payload
-  await client.chat.postEphemeral({
-    text: transcript('cave-join', { user }),
-    channel: transcript('channels.cave'),
-    user,
-  })
 
-  await sleep(1000)
+  await Promise.all([
+    client.chat.postMessage({
+      channel: user,
+      blocks: [{
+        "type": "divider"
+      }]
+    }),
+    client.chat.postEphemeral({
+      text: transcript('cave-join', { user }),
+      channel: transcript('channels.cave'),
+      user,
+    })
+  ])
 
-  await client.chat.postMessage({
-    text: transcript('house.hello'),
-    // icon_url: transcript('avatar.default'),
-    channel: user,
-  })
+  await Promise.all([
+    await sleep(1000),
+    await client.chat.postMessage({
+      text: transcript('house.hello'),
+      // icon_url: transcript('avatar.default'),
+      channel: user,
+    })
+  ])
 
-  await sleep(1000)
+  await sleep(3000)
 
   await client.chat.postMessage({
     text: transcript('house.venture'),
     // icon_url: transcript('avatar.sad'),
     channel: user,
   })
-  await client.chat.postMessage({
-    text: transcript('house.beforeGo'),
-    // icon_url: transcript('avatar.default'),
-    channel: user,
-  })
 
-  await inviteUserToChannel(
-    client,
-    user,
-    transcript('channels.slack-themes'),
-    true
-  )
-  await sleep(1000)
+  await Promise.all([
+    sleep(3000),
+    client.chat.postMessage({
+      text: transcript('house.beforeGo'),
+      // icon_url: transcript('avatar.default'),
+      channel: user,
+    })
+  ])
+
+  await Promise.all([
+    client.chat.postMessage({
+      text: transcript('house.theme'),
+      // icon_url: transcript('avatar.default'),
+      channel: user,
+    }),
+    inviteUserToChannel(
+      client,
+      user,
+      transcript('channels.slack-themes'),
+      true
+    )
+  ])
+
+  await Promise.all([
+    sleep(3000),
+    client.chat.postMessage({
+      text: transcript('house.theme-invite'),
+      channel: user,
+    })
+  ])
 
   await client.chat.postMessage({
-    text: transcript('house.theme'),
-    // icon_url: transcript('avatar.default'),
-    channel: user,
-  })
-
-  await client.chat.postMessage({
+    text: "I'm done with colors",
     blocks: [
       transcript('block.single-button', {
-        text: 'Continue',
+        text: "I'm done with colors",
         value: 'theme_complete',
       }),
     ],
     // icon_url: transcript('avatar.log'),
     channel: user,
   })
-
-  // await client.chat.postMessage({
-  //   text: transcript('house.basement'),
-  //   blocks: [
-  //     {
-  //       type: 'context',
-  //       elements: [
-  //         {
-  //           type: 'mrkdwn',
-  //           text: transcript('house.basement'),
-  //         },
-  //       ],
-  //     },
-  //   ],
-  //   // icon_url: transcript('avatar.sad'),
-  //   channel: user,
-  // })
 }
 module.exports = { joinCaveInteraction }
