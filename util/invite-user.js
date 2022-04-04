a18e2ed60469df2f0091703975aa2ec4b79dc97e
Max Wofford <max@maxwofford.com>
2022-04-04 11:58:45 -0400

    Additional context on invite-user api

--- util/invite-user.js
+++ util/invite-user.js
@@ -7,6 +7,12 @@ async function inviteUser(email) {
   const channels = [transcript('channels.cave')]
   const customMessage =
     'While wandering through a forest, you stumble upon a cave...'
+
+  // @msw this is an undocumented Slack endpoint, not to be confused with
+  // https://api.slack.com/methods/admin.users.invite. The SLACK_LEGACY_TOKEN is
+  // a `xoxp` deprecated legacy token, which can no longer be generated:
+  // https://api.slack.com/legacy/custom-integrations/legacy-tokens
+
   const params = [
     `email=${email}`,
     `token=${process.env.SLACK_LEGACY_TOKEN}`,
