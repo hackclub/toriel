cec77abe3b9a51ce798b4777a1f3570b302ff9fe
Max Wofford <max@maxwofford.com>
2022-04-06 15:16:33 -0400

    Include default values so regular users can test the flow

--- interactions/post-welcome-committee.js
+++ interactions/post-welcome-committee.js
@@ -5,14 +5,21 @@ async function postWelcomeCommittee(client, user) {
   try {
     const slackuser = await client.users.info({ user })
     const email = slackuser?.user?.profile?.email
-    const invite = await prisma.invite.findFirst({ where: { email }, orderBy: { createdAt: "desc"} })
+    const invite = await prisma.invite.findFirst({
+      where: { email },
+      orderBy: { createdAt: 'desc' },
+    })
+    const message =
+      invite?.['welcome_message'] || "I'm using the /toriel-restart command"
+    const continent = invite?.['continent'] || 'DEFAULT_CONTINENT'
+    const hs = invite?.['high_school'] || true
     await client.chat.postMessage({
       channel: transcript('channels.welcome-committee'),
       text: transcript('welcome-committee', {
         user,
-        message: invite['welcome_message'],
-        continent: invite['continent'],
-        hs: invite['high_school'],
+        message,
+        continent,
+        hs,
       }),
     })
   } catch (e) {
