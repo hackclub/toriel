2e5f37004d6dcbe99a4463fbc6c44ac7bcc12481
celeste drummond <parafactual@gmail.com>
2022-09-26 17:55:57 -0400

    Add sleep between final Toriel posts

--- index.js
+++ index.js
@@ -175,6 +175,7 @@ const addToChannels = async (user) => {
     text: transcript('house.projects', { link }),
     channel: user,
   })
+  await sleep(1000)
   await client.chat.postMessage({
     text: transcript('house.assemble-video'),
     channel: user,
