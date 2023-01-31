c30c6738ebbdaac7a3b0255d1d98dd06588d03cd
Sam Poder <sam@hackclub.com>
2023-01-31 10:07:11 +0800

    Create start-from-clippy.js

---
+++ endpoints/start-from-clippy.js
@@ -0,0 +1,18 @@
+// the user joins #the-cave, the starter channel
+const { sleep } = require('../util/sleep')
+const { transcript } = require('../util/transcript')
+const { prisma } = require('../db')
+const { getEmailFromUser } = require('../util/get-invite')
+
+module.exports = async function ping(req, res) {
+  const {  user_id } = req.query
+  await Promise.all([
+    client.chat.postEphemeral({
+      text: transcript('announcements-to-cave'),
+      channel: transcript('channels.announcements'),
+      user: user_id
+    }),
+  ])
+  res.json({ pong: true })
+}
+module.exports = { joinCaveInteraction }
