46ef3473e2e204503080e44a8160b5715c4d0809
Sam Poder <sam@hackclub.com>
2023-01-31 10:19:53 +0800

    Try / Catch

--- endpoints/start-from-clippy.js
+++ endpoints/start-from-clippy.js
@@ -4,12 +4,15 @@ const { client } = require('../app')
 
 module.exports = async function startFromClippy(req, res) {
   const { user_id } = req.query
-  await Promise.all([
-    client.chat.postEphemeral({
-      text: transcript('announcements-to-cave'),
-      channel: transcript('channels.announcements'),
-      user: user_id,
-    }),
-  ])
+  try {
+    await Promise.all([
+      client.chat.postEphemeral({
+        text: transcript('announcements-to-cave'),
+        channel: transcript('channels.announcements'),
+        user: user_id,
+      }),
+    ])
+  }
+  catch (e) {}
   res.json({ pong: true })
 }
