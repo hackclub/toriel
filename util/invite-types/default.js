daadc4181265438d0d6886c7765fa06ff90a45e3
Arav Narula <arav.1tb@gmail.com>
2023-11-30 11:05:22 -0500

    add waiting room

--- util/invite-types/default.js
+++ util/invite-types/default.js
@@ -1,7 +1,7 @@
 const { transcript } = require('../transcript')
 
 const defaultInvite = {
-  channels: [transcript('channels.cave')],
+  channels: [transcript('channels.cave'), transcript('channels.waiting-room')],
   customMessage: 'While wandering through a forest, you stumble upon a cave...',
 }
 
