3ba76fdab25ec5515ea6f7225d605fb89776cf3a
Arav Narula <arav.1tb@gmail.com>
2023-11-30 10:32:47 -0500

    Create hcb.js

---
+++ util/invite-types/hcb.js
@@ -0,0 +1,8 @@
+const { transcript } = require('../transcript')
+
+const hcbInvite = {
+  channels: [transcript('channels.bank')],
+  customMessage: 'While wandering through a forest, you stumble upon a cave...',
+}
+
+module.exports = { hcbInvite }
