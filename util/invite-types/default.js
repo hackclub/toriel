c38f9e4dd63a96f27698aa12b7cbfd3435d3aa75
Arav <arav.1tb@gmail.com>
2023-12-10 13:56:21 -0500

    changes

--- util/invite-types/default.js
+++ util/invite-types/default.js
@@ -1,7 +1,7 @@
 const { transcript } = require('../transcript')
 
 const defaultInvite = {
-  channels: [transcript('channels.cave'), transcript('channels.waiting-room')],
+  channels: [transcript('channels.cave')],
   customMessage: 'While wandering through a forest, you stumble upon a cave...',
 }
 
