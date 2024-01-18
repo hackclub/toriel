cf06e8c286612ffab98413163b9282be9c637994
Max Wofford <max@maxwofford.com>
2024-01-18 16:11:39 -0500

    Add shaded spot as a default invited channel

--- util/invite-types/default.js
+++ util/invite-types/default.js
@@ -1,7 +1,7 @@
 const { transcript } = require('../transcript')
 
 const defaultInvite = {
-  channels: [transcript('channels.cave')],
+  channels: [transcript('channels.cave'), transcript('channels.shaded-spot-under-a-large-tree')],
   customMessage: 'While wandering through a forest, you stumble upon a cave...',
 }
 
