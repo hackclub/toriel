0ceb56c2a54fe0600a158041251f6b1b89b896cc
Max Wofford <max@maxwofford.com>
2024-01-18 16:24:11 -0500

    Format with prettier

--- util/invite-types/onboard.js
+++ util/invite-types/onboard.js
@@ -1,7 +1,11 @@
 const { transcript } = require('../transcript')
 
 const onboardInvite = {
-  channels: [transcript('channels.onboard'), transcript('channels.cave'), transcript('channels.shaded-spot-under-a-large-tree')],
+  channels: [
+    transcript('channels.onboard'),
+    transcript('channels.cave'),
+    transcript('channels.shaded-spot-under-a-large-tree'),
+  ],
   customMessage: 'Welcome onboard!',
 }
 
