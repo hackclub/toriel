e5f3a8455a44b09da94f313b917e614c4fc3d78e
Max Wofford <max@maxwofford.com>
2024-01-18 19:10:50 -0500

    Formatting

--- interactions/ping-user.js
+++ interactions/ping-user.js
@@ -1,7 +1,6 @@
 // Sends an ephemeral message to a user in the #cave channel right after they join
 
 async function pingUserInteraction({ user }) {
-
   await client.chat.postEphemeral({
     text: transcript('cave-ping', { user }),
     channel: transcript('channels.cave'),
