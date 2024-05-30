e6bdcfbd622053253d938ccccf31c1a8e3f71eae
David M <62346025+aboutdavid@users.noreply.github.com>
2024-05-30 18:28:41 -0400

    remove twilio

--- util/alert.js
+++ util/alert.js
@@ -20,7 +20,7 @@ module.exports = {
     })
   },
   /**
-   * Sends an urgent alert to Slack and Twilio
+   * Sends an urgent alert to Slack and PagerDuty
    * @param {Object} options - The options for the alert.
    * @param {string} options.summary - The summary of the alert (for notifications)
    * @param {string} options.detailed - The detailed information of the alert.
@@ -33,24 +33,5 @@ module.exports = {
         text: detailed,
       }),
     })
-    if (process.env.TWILIO_ACCOUNT_SID && process.env.TWILIO_AUTH_TOKEN) {
-      const client = require('twilio')(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_AUTH_TOKEN
-      )
-      process.env.TWILIO_TO.split(',').forEach((phoneNumber, index, array) => {
-        client.messages
-          .create({
-            body: summary,
-            from: process.env.TWILIO_FROM,
-            to: phoneNumber,
-          })
-          .then((message) => {
-            console.info(
-              `Sent urgent alert to user ${index + 1}/${array.length}: ${message.sid}`
-            )
-          })
-      })
-    }
   },
 }
