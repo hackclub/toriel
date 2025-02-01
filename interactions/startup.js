d5cec3a9807d922efbc02df88e89ebb7954e7fd4
Arav Narula <arav.1tb@gmail.com>
2025-02-01 15:33:44 -0500

    remove heroku logs

--- interactions/startup.js
+++ interactions/startup.js
@@ -5,24 +5,12 @@ const { metrics } = require('../util/metrics')
 function getEnv() {
   return process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 }
-function getVersion() {
-  // https://devcenter.heroku.com/articles/dyno-metadata
-  const versionData = [
-    process.env.HEROKU_RELEASE_VERSION,
-    process.env.HEROKU_SLUG_COMMIT,
-  ]
-  if (versionData) {
-    return versionData.map((t) => t.slice(0, 8)).join('-')
-  } else {
-    return 'local'
-  }
-}
+
 
 async function startup() {
   await client.chat.postMessage({
     blocks: [
       transcript('block.text', { text: transcript('startup.message') }),
-      transcript('block.context', { text: `${getEnv()}-${getVersion()}` }),
     ],
     channel: transcript('channels.bot-spam'),
     username: 'TUTORIEL',
